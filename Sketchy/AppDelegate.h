4e4652f20dad149ceab65118a8577eb70f9e670e
Ben Dodson <bjdodson@gmail.com>
2013-03-02 11:16:26 -0800

    Initial Commit

---
+++ Sketchy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sketchy
+//
+//  Created by Ben Dodson on 3/2/13.
+//  Copyright (c) 2013 MobiSocial Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sketchy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Sketchy
+//
+//  Created by Ben Dodson on 3/2/13.
+//  Copyright (c) 2013 MobiSocial Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
